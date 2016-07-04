059dc01c7abb49e82d81646a0734f2758e3e872a
duhaodong <du.haodong@xiaoyi.com>
2016-07-04 20:05:10 +0800

    Initial Commit

---
+++ pcm2aac/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  pcm2aac
+//
+//  Created by duhaodong on 16/7/4.
+//  Copyright © 2016年 duhaodong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
